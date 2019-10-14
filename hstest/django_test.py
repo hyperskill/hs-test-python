b1efe55a36d5a21658499748fbdef586e615c01f
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-10-14 10:30:37 +0300

    Add django example

--- hstest/django_test.py
+++ hstest/django_test.py
@@ -1,7 +1,12 @@
 import os
 import signal
 import subprocess
+from time import sleep
+from urllib.error import URLError, HTTPError
+from urllib.request import urlopen
 from hstest.stage_test import StageTest
+from hstest.check_result import CheckResult
+from hstest.test_case import TestCase
 
 
 class DjangoTest(StageTest):
@@ -16,3 +21,59 @@ class DjangoTest(StageTest):
     def after_all_tests(self):
         if self.process is not None:
             self._kill(self.process.pid, signal.SIGINT)
+
+
+class ExampleDjangoTest(DjangoTest):
+
+    def get_main_page(self) -> CheckResult:
+        for _ in range(3):
+            try:
+                main_page = urlopen('http://localhost:8000').read().decode()
+                if 'Welcome to the Ticket service' in main_page:
+                    return CheckResult.true()
+                return CheckResult.false(
+                    'Main page should contain "Welcome to the Ticket service" line'
+                )
+            except (URLError, HTTPError):
+                sleep(2)
+        return CheckResult.false(
+            'Cannot connect to the main page.'
+        )
+
+    def get_ticket_page(self) -> CheckResult:
+        try:
+            ticket_page = urlopen('http://localhost:8000/tickets').read().decode()
+        except (URLError, HTTPError):
+            return CheckResult.false(
+                'Cannot connect to the ticket page "/tickets".'
+            )
+
+        if 'Available tickets' in ticket_page:
+            return CheckResult.true()
+        return CheckResult.false(
+            'Tickets page should contain "Available tickets" line'
+        )
+
+    def get_about_page(self) -> CheckResult:
+        try:
+            about_page = urlopen('http://localhost:8000/about').read().decode()
+        except (URLError, HTTPError):
+            return CheckResult.false(
+                'Cannot connect to the about page "/about".'
+            )
+
+        if 'About' in about_page:
+            return CheckResult.true()
+        return CheckResult.false(
+            'About page should contain "About" line'
+        )
+
+    def generate(self):
+        return [
+            TestCase(attach=self.get_main_page),
+            TestCase(attach=self.get_ticket_page),
+            TestCase(attach=self.get_about_page),
+        ]
+
+    def check(self, reply, attach):
+        return attach()
