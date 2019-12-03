04d735de1b3a92f6822ffdb22e40e6f84aa64c7c
pavelperc <pavelperc@mail.ru>
2019-12-03 22:08:22 +0300

    added setup.py to create a pip package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="hstest",
+    version="1.0.0",
+    author="Valdimir Turov",
+    author_email="vladimir.turov@stepik.org",
+    description="A small framework that simplifies testing educational projects for https://hyperskill.org/.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyperskill/hs-test-python",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ],
+)
