31fd48b14b8c9cb6faa07bdbd222ba32b86ce5d7
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-01-24 20:37:33 +0300

    Add tests on command line args

---
+++ tests/outcomes/test_command_line_arguments_passing/test.py
@@ -0,0 +1,29 @@
+import unittest
+from typing import Any, List
+
+from hstest.check_result import CheckResult
+from hstest.stage_test import StageTest
+from hstest.test_case import TestCase
+
+
+class TestCommendLineArgumentsPassing(StageTest):
+
+    def generate(self) -> List[TestCase]:
+        return [
+            TestCase(
+                attach='4\n-in\n123\nout\n234\n',
+                args=['-in', '123', 'out', '234']
+            ),
+        ]
+
+    def check(self, reply: str, attach: Any) -> CheckResult:
+        return CheckResult(reply == attach, '')
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        status, feedback = TestCommendLineArgumentsPassing(
+            'tests.outcomes.test_command_line_arguments_passing.program'
+        ).run_tests()
+        self.assertEqual(status, 0)
+        self.assertEqual(feedback, 'test OK')
