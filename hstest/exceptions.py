b4f4b0304290dd10e634cab91069172d7dcc7e0c
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-01-24 21:42:49 +0300

    Add exceptions

--- hstest/exceptions.py
+++ hstest/exceptions.py
@@ -1,4 +1,3 @@
-
 class BadSolutionException(Exception):
     def __init__(self, message):
         self.message = message
@@ -12,3 +11,28 @@ class SyntaxException(BadSolutionException):
 class ExitException(BadSolutionException):
     def __init__(self, message):
         super().__init__(message)
+
+
+class ExceptionWithFeedback(Exception):
+    def __init__(self, error_text: str, real_exception: BaseException):
+        self.error_text: str = error_text
+        self.real_exception: BaseException = real_exception
+
+
+class FatalErrorException(Exception):
+    def __init__(self, error_text: str):
+        self.error_text = error_text
+
+
+class TestPassedException(Exception):
+    pass
+
+
+class TimeLimitException(Exception):
+    def __init__(self, time_limit_ms: int):
+        self.time_limit_ms: int = time_limit_ms
+
+
+class WrongAnswerException(Exception):
+    def __init__(self, error_text: str):
+        self.error_text = error_text
