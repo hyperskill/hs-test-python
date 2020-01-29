3e3e244680d023d7ef0840e29f198046f9963ece
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-01-29 21:08:12 +0300

    Add test on curr test case

---
+++ tests/outcomes/test_curr_test_case/test.py
@@ -0,0 +1,35 @@
+import unittest
+import textwrap
+from typing import Any, List
+
+from hstest.check_result import CheckResult
+from hstest.stage_test import StageTest
+from hstest.test_case import TestCase
+from hstest.test_run import TestRun
+
+
+class TestOpenFileAndNotClose(StageTest):
+    tc_1 = None
+    tc_2 = None
+
+    def generate(self) -> List[TestCase]:
+        self.tc_1 = TestCase(stdin='1', attach=1)
+        self.tc_2 = TestCase(stdin='2', attach=2)
+        return [self.tc_1, self.tc_2]
+
+    def check(self, reply: str, attach: Any) -> CheckResult:
+        tc = TestRun.curr_test_run.test_case
+        if (tc.input == '1' and tc.attach == 1 and tc is self.tc_1 or
+                tc.input == '2' and tc.attach == 2 and tc is self.tc_2):
+            return CheckResult.true()
+        return CheckResult.false()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        status, feedback = TestOpenFileAndNotClose(
+            'tests.outcomes.test_curr_test_case.program'
+        ).run_tests()
+
+        self.assertEqual('test OK', feedback)
+        self.assertEqual(status, 0)
