0f5bedfd35a83c50dd89234e32422e6097244f7c
Vladimir Turov <aaaaaa2493@yandex.ru>
2020-09-10 02:15:48 +0300

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="hs-test-python",
-    version="2.0.1",
+    version="3.0.0",
     author="Vladimir Turov",
     author_email="vladimir.turov@stepik.org",
     description="A small framework that simplifies testing educational projects for https://hyperskill.org/.",
