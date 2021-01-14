bce6e295f5bd201f5c1b4726e0fc0a0381580f60
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-01-14 15:50:10 +0300

    Make variables private

--- hstest/stage/matplotlib_test.py
+++ hstest/stage/matplotlib_test.py
@@ -5,8 +5,8 @@ class MatplotlibTest(StageTest):
     def __init__(self, source: str = ''):
         super().__init__(source)
 
-        self._all_figures = []
-        self._new_figures = []
+        self.__all_figures = []
+        self.__new_figures = []
 
         import matplotlib
         import matplotlib.pyplot as plt
@@ -16,18 +16,18 @@ class MatplotlibTest(StageTest):
         def custom_show_func(*args, **kwargs):
             managers = Gcf.get_all_fig_managers()
             for m in managers:
-                self._all_figures.append(m.canvas.figure)
-                self._new_figures.append(m.canvas.figure)
+                self.__all_figures.append(m.canvas.figure)
+                self.__new_figures.append(m.canvas.figure)
                 Gcf.destroy(m.num)
 
         plt.show = custom_show_func
 
     @property
     def new_figures(self):
-        result = self._new_figures
-        self._new_figures = []
+        result = self.__new_figures
+        self.__new_figures = []
         return result
 
     @property
     def all_figures(self):
-        return self._all_figures
+        return self.__all_figures
