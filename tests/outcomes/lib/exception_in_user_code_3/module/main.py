4ff8bb761136c008788154b6c441436c41d80e54
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-01-21 22:33:47 +0300

    Add test on exception

---
+++ tests/outcomes/lib/exception_in_user_code_3/module/main.py
@@ -0,0 +1,1 @@
+raise_error_here
---
+++ tests/outcomes/lib/exception_in_user_code_3/test.py
@@ -0,0 +1,35 @@
+import unittest
+from inspect import cleandoc
+from typing import Any, List
+
+from hstest.check_result import CheckResult
+from hstest.stage_test import StageTest
+from hstest.test_case import TestCase
+
+
+class ExceptionInUserCodeTest3(StageTest):
+
+    def generate(self) -> List[TestCase]:
+        return [TestCase()]
+
+    def check(self, reply: str, attach: Any) -> CheckResult:
+        return CheckResult.correct()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        status, feedback = ExceptionInUserCodeTest3('module.main').run_tests()
+
+        self.assertEqual(cleandoc(
+            """
+            Exception in test #1
+
+            Traceback (most recent call last):
+              File "main.py", line 1, in <module>
+                raise_error_here
+            NameError: name 'raise_error_here' is not defined
+            """), feedback)
+
+
+if __name__ == '__main__':
+    Test().test()
