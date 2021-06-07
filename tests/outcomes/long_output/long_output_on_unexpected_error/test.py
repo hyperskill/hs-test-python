56383f32368ff9500987378328fe7b29e23e383d
Ainur Gimadeev <inuur@mail.ru>
2021-06-07 16:40:56 +0300

    Replace lists with for loop

--- tests/outcomes/long_output/long_output_on_unexpected_error/test.py
+++ tests/outcomes/long_output/long_output_on_unexpected_error/test.py
@@ -21,8 +21,12 @@ class Test(unittest.TestCase):
 
         status, feedback = TestWrongOutputWithTooLongOutput('main').run_tests()
 
-        [self.assertIn(correct_line, feedback) for correct_line in correct_lines]
-        [self.assertNotIn(wrong_line, feedback) for wrong_line in wrong_lines]
+        for correct_line in correct_lines:
+            self.assertIn(correct_line, feedback)
+
+        for wrong_line in wrong_lines:
+            self.assertNotIn(wrong_line, feedback)
+
         self.assertEqual(status, -1)
 
 
