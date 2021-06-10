e5285540f73c36bba5950a0a52bdbdbad27ad6d6
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-06-10 22:07:46 +0300

    Add other tests for Go

---
+++ tests/projects/go/coffee_machine/stage1_ce/tests.py
@@ -0,0 +1,43 @@
+import unittest
+
+from hstest.stage_test import *
+from hstest.test_case import TestCase
+
+CheckResult.correct = lambda: CheckResult(True, '')
+CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
+
+OUTPUT = """
+Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!
+"""
+
+
+class CoffeeMachineTest(StageTest):
+    def generate(self) -> List[TestCase]:
+        return TestCase.from_stepik([('', OUTPUT)])
+
+    def check(self, reply: str, clue: Any) -> CheckResult:
+        return CheckResult(
+            reply.strip() == clue.strip(),
+            'You should make coffee exactly like in the example')
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        status, feedback = CoffeeMachineTest().run_tests()
+
+        self.assertEqual(feedback, '''Compilation error
+
+.\main.go:4:2: imported and not used: "fmt"
+.\main.go:8:2: undefined: Println''')
+
+        self.assertNotEqual(status, 0)
+
+
+if __name__ == '__main__':
+    Test().test()
