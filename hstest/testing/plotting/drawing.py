a3447f74f2ed6f7e9f856483a8037fd930775add
Ainur Gimadeev <inuur@mail.ru>
2021-11-17 14:39:43 +0300

    Add classes with library and graphic types

--- hstest/testing/plotting/drawing.py
+++ hstest/testing/plotting/drawing.py
@@ -3,3 +3,31 @@ class Drawing:
         self.library = library
         self.type = plot_type
         self.data = data
+
+
+class DrawingLibrary:
+    matplotlib = "matplotlib"
+    pandas = "pandas"
+    seaborn = "seaborn"
+
+
+class DrawingType:
+    hist = "hist"
+    line = "line"
+    scatter = "scatter"
+    pie = "pie"
+    bar = "bar"
+    violin = "violin"
+    heatmap = "heatmap"
+    box = "box"
+    dis = "dis"  # distribution plot (pandas' density, seaborn' displot)
+
+    # pandas
+    kde = "kde"
+    density = "density"
+    area = "area"
+    hexbin = "hexbin"
+
+    # seaborn
+    lm = "lm"
+    cat = "cat"
