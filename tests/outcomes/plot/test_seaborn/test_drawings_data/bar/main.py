843fe231501fd795209072300c9cd92f1c629b07
Ainur Gimadeev <inuur@mail.ru>
2021-11-19 15:11:12 +0300

    Add bar support

---
+++ tests/outcomes/plot/test_seaborn/test_drawings_data/bar/main.py
@@ -0,0 +1,23 @@
+def plot():
+    try:
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+        import pandas as pd
+        import numpy as np
+    except ModuleNotFoundError:
+        return
+
+    df = pd.DataFrame(np.array([1, 2, 3, 4, 5]), columns=['one'])
+
+    sns.histplot(
+        df, y="one"
+    )
+
+    sns.histplot(
+        df, x="one"
+    )
+
+    plt.show()
+
+
+plot()
