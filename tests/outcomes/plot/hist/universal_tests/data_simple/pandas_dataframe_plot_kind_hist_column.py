03f46a122d6b3e4556e030e6a8bc83ceae40b775
Vladimir Turov <aaaaaa2493@yandex.ru>
2022-01-31 17:46:28 +0300

    Add more histogram tests

---
+++ tests/outcomes/plot/hist/universal_tests/data_simple/pandas_dataframe_plot_kind_hist_column.py
@@ -0,0 +1,17 @@
+def plot():
+    try:
+        import pandas as pd
+        import numpy as np
+        import matplotlib.pyplot as plt
+    except ModuleNotFoundError:
+        return
+
+    df = pd.DataFrame(np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]),
+                      columns=['one', 'two'])
+
+    df.plot(column='one', kind='hist')
+
+    plt.show()
+
+
+plot()
