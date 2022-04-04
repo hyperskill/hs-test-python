576c589b88a4aeaa19fc024699a211d19511bcae
Ainur Gimadeev <inuur@mail.ru>
2022-04-04 12:45:00 +0300

    SQL tests

--- hstest/stage/sql_test.py
+++ hstest/stage/sql_test.py
@@ -1,28 +1,30 @@
 import os
+import re
+import sqlite3
 
-from exception.outcomes import ErrorWithFeedback, WrongAnswer
+from exception.outcomes import ErrorWithFeedback
 from hstest.stage.stage_test import StageTest
 from hstest.testing.runner.sql_runner import SQLRunner
-import re
 
 
 class SQLTest(StageTest):
     runner = SQLRunner()
     queries = dict()
 
-    def run_tests(self, *, debug=False, is_unittest: bool = False):
+    def __init__(self, args='', *, source: str = ''):
         self.find_sql_files()
-        return super(SQLTest, self).run_tests()
+        self.db = sqlite3.connect(':memory:')
+        super().__init__(args, source=source)
 
     def find_sql_files(self):
         root_folder = os.getcwd()
-
         is_sql_file_found: bool = False
 
         for file in os.listdir(root_folder):
             if file.endswith('.sql'):
                 is_sql_file_found = True
                 self.parse_sql_file(file)
+
         if not is_sql_file_found:
             raise ErrorWithFeedback("Can't find any SQL file!")
 
