2f985ce05aa03e2ada9bd705c86c4b8a1fdaab56
inuur <inuur@mail.ru>
2022-10-09 11:09:37 +0300

    test actions

--- tests/testing.py
+++ tests/testing.py
@@ -8,13 +8,6 @@ from os import listdir
 from os.path import dirname, isdir, isfile
 from typing import List
 from unittest import TestCase, TestLoader, TestSuite, TextTestRunner
-
-
-print(os.path.exists('./tests/outcomes/plot/bar/pandas/test_example/ab_test.csv'))
-print(os.path.exists('./tests/outcomes/plot/bar/pandas/test_example/test.py'))
-print(os.getcwd())
-os.system('ls -all')
-
 from hstest.common import utils as hs
 from hstest.dynamic.output.colored_output import GREEN_BOLD, RED_BOLD, RESET
 
