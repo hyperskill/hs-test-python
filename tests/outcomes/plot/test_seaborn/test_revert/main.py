4e1d3169f6f566fd454f72c868ed7983f01d8694
inuur <inuur@mail.ru>
2022-10-09 11:42:53 +0300

    test actions

--- tests/outcomes/plot/test_seaborn/test_revert/main.py
+++ tests/outcomes/plot/test_seaborn/test_revert/main.py
@@ -18,31 +18,31 @@ def plot():
         df, x="flipper_length_mm"
     )
 
-    # sns.lineplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
-    # sns.lmplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
-    # sns.scatterplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
-    #
-    # sns.catplot(
-    #     data=df, kind="bar",
-    #     x="species", y="body_mass_g", hue="sex",
-    #     ci="sd", palette="dark", alpha=.6, height=6
-    # )
-    #
-    # x = np.array(list("ABCDEFGHIJ"))
-    # y1 = np.arange(1, 11)
-    # sns.barplot(x=x, y=y1, palette="rocket", ax=ax)
-    #
-    # sns.violinplot(data=df, palette="Set3", bw=.2, cut=1, linewidth=1)
-    #
-    # flights_long = sns.load_dataset("flights")
-    # flights = flights_long.pivot("month", "year", "passengers")
-    # sns.heatmap(flights, annot=True, fmt="d", linewidths=.5, ax=ax)
-    #
-    # tips = sns.load_dataset("tips")
-    # sns.boxplot(x="day", y="total_bill",
-    #             hue="smoker", palette=["m", "g"],
-    #             data=tips)
-    # sns.despine(offset=10, trim=True)
+    sns.lineplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
+    sns.lmplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
+    sns.scatterplot(data=df, x="flipper_length_mm", y="flipper_length_mm")
+
+    sns.catplot(
+        data=df, kind="bar",
+        x="species", y="body_mass_g", hue="sex",
+        ci="sd", palette="dark", alpha=.6, height=6
+    )
+
+    x = np.array(list("ABCDEFGHIJ"))
+    y1 = np.arange(1, 11)
+    sns.barplot(x=x, y=y1, palette="rocket", ax=ax)
+
+    sns.violinplot(data=df, palette="Set3", bw=.2, cut=1, linewidth=1)
+
+    flights_long = sns.load_dataset("flights")
+    flights = flights_long.pivot("month", "year", "passengers")
+    sns.heatmap(flights, annot=True, fmt="d", linewidths=.5, ax=ax)
+
+    tips = sns.load_dataset("tips")
+    sns.boxplot(x="day", y="total_bill",
+                hue="smoker", palette=["m", "g"],
+                data=tips)
+    sns.despine(offset=10, trim=True)
 
     import matplotlib.pyplot as plt
 
--- tests/outcomes/plot/test_seaborn/test_revert/test.py
+++ tests/outcomes/plot/test_seaborn/test_revert/test.py
@@ -12,16 +12,16 @@ class TestSeaborn(PlottingTest):
     def test(self):
         import matplotlib
 
-        SeabornHandler.revert_plots()
-        backend = matplotlib.get_backend()
-        matplotlib.use('Agg')
+        # SeabornHandler.revert_plots()
+        # backend = matplotlib.get_backend()
+        # matplotlib.use('Agg')
 
         program = TestedProgram()
         program.start()
 
-        matplotlib.use(backend)
-
-        time.sleep(4)
+        # matplotlib.use(backend)
+        #
+        # time.sleep(4)
 
         if len(self.all_figures()) != 0:
             return wrong(f'Expected 0 plots to be plotted using matplotlib library, found {len(self.all_figures())}')
