4e533557408855ffb8f9037e21c18d625705e1c8
Ainur Gimadeev <inuur@mail.ru>
2022-10-11 11:14:39 +0300

    fix updating sys.path

--- tests/testing.py
+++ tests/testing.py
@@ -7,8 +7,6 @@ from os import listdir
 from os.path import isfile, isdir, dirname, abspath
 from typing import List
 from unittest import TestCase, TestLoader, TestSuite, TextTestRunner
-from hstest.common import utils as hs
-from hstest.dynamic.output.colored_output import GREEN_BOLD, RED_BOLD, RESET
 
 content_path = dirname(
     dirname(abspath(__file__))
@@ -16,6 +14,9 @@ content_path = dirname(
 sys.path.insert(0, content_path)
 
 print(sys.path)
+
+from hstest.common import utils as hs
+from hstest.dynamic.output.colored_output import GREEN_BOLD, RED_BOLD, RESET
 
 
 class OutputForTest:
@@ -54,7 +55,7 @@ class UnitTesting:
 
         for module in UnitTesting.find_modules(dirname(__file__)):
             if 'outcomes' in module and not module.endswith('.test') or \
-                    'projects' in module and not module.endswith('.tests'):
+                'projects' in module and not module.endswith('.tests'):
                 continue
             try:
                 imported = import_module(f'tests.{module}')
