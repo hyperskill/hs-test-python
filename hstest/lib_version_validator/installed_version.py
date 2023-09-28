a483345f215b746f0d48a7647bfc3a020c139cbd
Sergey <polischuk0107@gmail.com>
2023-09-28 16:00:43 +0300

    reformat

--- hstest/lib_version_validator/installed_version.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-
-def get_installed_version():
-    library_name = "hs-test-python"
-    installed_library_version = pkg_resources.get_distribution(library_name).version
-
-    return installed_library_version
