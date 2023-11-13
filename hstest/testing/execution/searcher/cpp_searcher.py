24853565c11c5d7deb2b63f8ef27b402f821835c
Sergey <polischuk0107@gmail.com>
2023-11-13 18:56:50 +0200

    added cpp executor and cpp searcher (#44)
    * add cpp executor
    
    * reformat
    
    * reformat
    
    * reformat
    
    * reformat
    
    * reformat
    
    * reformat
    
    * reformat cpp_executor.py
    
    * improved regular expression cpp_searcher.py
    
    * Update cpp_executor.py
    
    reformat compilation command
    
    * Update hstest/testing/execution/process/cpp_executor.py
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>
    
    ---------
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>

---
+++ hstest/testing/execution/searcher/cpp_searcher.py
@@ -0,0 +1,25 @@
+import re
+
+from hstest.testing.execution.filtering.main_filter import MainFilter
+from hstest.testing.execution.runnable.runnable_file import RunnableFile
+from hstest.testing.execution.searcher.base_searcher import BaseSearcher
+
+
+class CppSearcher(BaseSearcher):
+
+    @property
+    def extension(self) -> str:
+        return '.cpp'
+
+    def search(self, where: str = None) -> RunnableFile:
+        main_func_searcher = re.compile(r'(^|\n)\s*int\s+main\s*\(.*\)', re.M)
+
+        return self._search(
+            where,
+            force_content_filters=[
+                MainFilter(
+                    'int main()',
+                    source=lambda s: main_func_searcher.search(s) is not None
+                ),
+            ]
+        )
