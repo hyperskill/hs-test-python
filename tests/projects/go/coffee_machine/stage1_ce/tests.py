2ab6df1f9a0c7445587ef19637be9f8cc741efb6
polischuks <polischuk0107@gmail.com>
2025-02-14 12:44:33 +0200

    Add type hints to test files for improved clarity and type checking

--- tests/projects/go/coffee_machine/stage1_ce/tests.py
+++ tests/projects/go/coffee_machine/stage1_ce/tests.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from hstest.stage_test import *
 from hstest.test_case import TestCase
 from hstest.testing.unittest.user_error_test import UserErrorTest
 import os
+from hstest.check_result import CheckResult
 
 CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
