df3c96d2cb6f4c122ec01879ae92eedf7e1ed987
polischuks <polischuk0107@gmail.com>
2025-02-15 14:47:43 +0200

    Refactor python_executor to use is_windows utility for OS checks

--- hstest/testing/execution/process/python_executor.py
+++ hstest/testing/execution/process/python_executor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 
+from hstest.common.os_utils import is_windows
 from hstest.testing.execution.process_executor import ProcessExecutor
 from hstest.testing.execution.searcher.python_searcher import PythonSearcher
 
@@ -10,12 +11,12 @@ class PythonExecutor(ProcessExecutor):
     def __init__(self, source_name: str | None = None) -> None:
         super().__init__(PythonSearcher().find(source_name))
         # Set UTF-8 encoding for Python I/O on Windows
-        if os.name == "nt":
+        if is_windows():
             os.environ["PYTHONIOENCODING"] = "utf8"
 
     def _execution_command(self, *args: str):
         cmd = ["python"]
-        if os.name == "nt":  # Works on all Windows versions (32/64 bit)
+        if is_windows():  # Works on all Windows versions (32/64 bit)
             # Set UTF-8 encoding for stdin/stdout on Windows
             cmd.extend(["-X", "utf8"])
         cmd.extend(["-u", self.runnable.file, *list(args)])
