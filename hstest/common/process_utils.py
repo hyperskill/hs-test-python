6c43f33a315059a638efe0a13caf16f1d7cee4ec
Igor Kirillov <63199449+KirillovProj@users.noreply.github.com>
2026-03-02 17:15:53 +0100

    Release 020326 (#155)
    * Bump minimal python version to 3.11, support python 3.14
    
    (cherry picked from commit e495b4177522514c1c26782aa059ce112abf6252)
    
    * Fixed thread shim on 3.14+
    
    (cherry picked from commit 882e8c4ea09d1fd0db5641eb502ee6c6472bf3cc)

--- hstest/common/process_utils.py
+++ hstest/common/process_utils.py
@@ -20,8 +20,6 @@ class DaemonThreadPoolExecutor(ThreadPoolExecutor):
         if self._idle_semaphore.acquire(timeout=0):
             return
 
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
         def weakref_cb(_, q=self._work_queue) -> None:
             q.put(None)
 
@@ -29,12 +27,20 @@ class DaemonThreadPoolExecutor(ThreadPoolExecutor):
         if num_threads < self._max_workers:
             thread_name = "%s_%d" % (self._thread_name_prefix or self, num_threads)
 
-            args = (
-                weakref.ref(self, weakref_cb),
-                self._work_queue,
-                self._initializer,
-                self._initargs,
-            )
+            # Python 3.14+
+            if hasattr(self, "_create_worker_context"):
+                args = (
+                    weakref.ref(self, weakref_cb),
+                    self._create_worker_context(),
+                    self._work_queue,
+                )
+            else:
+                args = (
+                    weakref.ref(self, weakref_cb),
+                    self._work_queue,
+                    self._initializer,
+                    self._initargs,
+                )
 
             t = threading.Thread(name=thread_name, target=_worker, args=args, group=self.group)
             t.daemon = True
